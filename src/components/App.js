850e3612a7e33a6231bcd36eb8d2bf2936bae8a2
Zach Lefler <leaving.valhalla@gmail.com>
2022-03-26 11:05:41 -0700

    finish deliverables

--- src/components/App.js
+++ src/components/App.js
@@ -7,20 +7,34 @@ function App() {
   const [pizzas, setPizzas] = useState('');
   const [topping, setTopping] = useState('');
   const [size, setSize] = useState('');
-  const [vegetarian, setVegetarian] = useState('');
+  const [vegetarian, setVegetarian] = useState(true);
+  const [id, setId] = useState(true);
+  const [update, setUpdate] = useState(false);
 
   useEffect(
     () =>
       fetch('http://localhost:3001/pizzas')
         .then((res) => res.json())
         .then((data) => setPizzas(data)),
-    []
+    [update]
   );
 
   function onEditClick(pizzaClicked) {
     setTopping(pizzaClicked.topping);
     setSize(pizzaClicked.size);
     setVegetarian(pizzaClicked.vegetarian);
+    setId(pizzaClicked.id);
+  }
+
+  function onVegChange() {
+    setVegetarian((vegetarian) => !vegetarian);
+  }
+
+  // i made a state just to get the page to update with useEffect:
+  // bad practice? better way?
+
+  function onUpdate() {
+    setUpdate((update) => !update);
   }
 
   return (
@@ -32,7 +46,9 @@ function App() {
         size={size}
         setSize={setSize}
         vegetarian={vegetarian}
-        setVegetarian={setVegetarian}
+        handleVegChange={onVegChange}
+        id={id}
+        onUpdate={onUpdate}
       />
       <PizzaList pizzas={pizzas} onEditClick={onEditClick} />
     </>
--- src/components/PizzaForm.js
+++ src/components/PizzaForm.js
@@ -1,15 +1,38 @@
 import React from 'react';
 
+// so many props! zooming out, is there a better way to do this?
+// it feels like to control the form it's all necessary.
+
 function PizzaForm({
   topping,
   setTopping,
   size,
   setSize,
   vegetarian,
-  setVegetarian,
+  handleVegChange,
+  id,
+  onUpdate,
 }) {
+  function handleSubmit(e) {
+    e.preventDefault();
+    const newPizza = {
+      id: id,
+      topping: topping,
+      size: size,
+      vegetarian: vegetarian,
+    };
+
+    fetch(`http://localhost:3001/pizzas/${id}`, {
+      method: 'PATCH',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(newPizza),
+    })
+      .then((res) => res.json())
+      .then(onUpdate);
+  }
+
   return (
-    <form onSubmit={null /*handle that submit*/}>
+    <form onSubmit={handleSubmit}>
       <div className="form-row">
         <div className="col-5">
           <input
@@ -27,28 +50,53 @@ function PizzaForm({
             name="size"
             onChange={(e) => setSize(e.target.value)}
           >
-            <option value="Small">Small</option>
-            <option value="Medium">Medium</option>
-            <option value="Large">Large</option>
+            {/* very clunky conditional statements: better way? */}
+            {size === 'Small' ? (
+              <option value="Small" selected>
+                Small
+              </option>
+            ) : (
+              <option value="Small">Small</option>
+            )}
+            {size === 'Medium' ? (
+              <option value="Medium" selected>
+                Medium
+              </option>
+            ) : (
+              <option value="Medium">Medium</option>
+            )}
+            {size === 'Large' ? (
+              <option value="Large" selected>
+                Large
+              </option>
+            ) : (
+              <option value="Large">Large</option>
+            )}
           </select>
         </div>
         <div className="col">
           <div className="form-check">
             <input
+              onChange={handleVegChange}
               className="form-check-input"
               type="radio"
               name="vegetarian"
               value="Vegetarian"
+              checked={vegetarian}
             />
+
             <label className="form-check-label">Vegetarian</label>
           </div>
           <div className="form-check">
             <input
+              onChange={handleVegChange}
               className="form-check-input"
               type="radio"
               name="vegetarian"
               value="Not Vegetarian"
+              checked={!vegetarian}
             />
+
             <label className="form-check-label">Not Vegetarian</label>
           </div>
         </div>
